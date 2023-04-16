fa04365a529506ffab40d179024721d9f9426583
minyoungshin <mins6649@gmail.com>
2023-04-16 13:44:37 -0400

    client created

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import '../App.css';
+
+function Home(){
+
+    return(
+        <div>
+            <p>Home Page: Display Content after login</p>
+        </div>
+    )
+}
+
+export default Home;
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import '../App.css';
+
+function SignUp(){
+
+    return(
+        <div>
+            <p>Signup page</p>
+        </div>
+    )
+}
+
+export default SignUp;
