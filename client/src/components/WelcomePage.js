3207ac7c02f220fe9b19eab6b05eca0bedf272c9
minyoungshin <mins6649@gmail.com>
2023-04-19 11:53:54 -0400

    front page populated

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
+// import React, {useState, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 
-import SignUp from './SignUp';
+// import SignUp from './SignUp';
 import Login from './Login';
 
 
